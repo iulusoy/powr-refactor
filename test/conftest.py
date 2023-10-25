8b43d135cc5770920ec0cea24b393166173cab75
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2023-10-25 13:01:19 +0200

    cleaner env set up (#9)
    * use conftest
    
    * add missing folder, refactor fixtures
    
    * copy .powrconfig to home
    
    * cleanup

---
+++ test/conftest.py
@@ -0,0 +1,57 @@
+import pytest
+import os
+import subprocess
+from pathlib import Path
+
+
+# get the path of this file and export variables accordingly
+@pytest.fixture(scope="session")
+def set_vars():
+    filedir = Path(__file__).parents[0]
+    powrdir = filedir.parents[0] / "powr"
+    os.environ["POWR_WORK"] = powrdir.as_posix()
+    os.environ["POWREXEPATH"] = (powrdir / "exe.dir").as_posix()
+    # create the tmp_2day folder if not exists
+    tmp_2day = powrdir / "tmp_2day"
+    if not os.path.exists(tmp_2day):
+        os.mkdir(tmp_2day)
+    return powrdir
+
+
+# inject path into powrconfig
+@pytest.fixture(scope="session")
+def inject_path(set_vars):
+    powrconfig_file = set_vars / "powrconfig"
+    with open(powrconfig_file, "r") as f:
+        powrconfig = f.read()
+    # now insert path in correct spot
+    temp = powrconfig.split("export POWR_WORK=")
+    powrconfig = temp[0] + "export POWR_WORK=" + set_vars.as_posix() + temp[1]
+    powrconfig_file = set_vars / ".powrconfig"
+    with open(powrconfig_file, "w") as f:
+        f.write(powrconfig)
+    os.system("cp ${POWR_WORK}/.powrconfig ${HOME}/.powrconfig")
+
+
+@pytest.fixture(scope="session")
+def get_chain(inject_path):
+    makechain_command = "${POWR_WORK}/proc.dir/makechain.com 1"
+    temp = subprocess.run(
+        makechain_command,
+        shell=True,
+        check=True,
+        executable="/bin/bash",
+        capture_output=True,
+        text=True,
+    )
+    print(temp.stdout)
+    print(temp.stderr)
+    yield "Created chain 1"
+    # teardown directories
+    # we need access to ${POWR_WORK} so shutil will not work
+    # why does sourcing powrconfig in the script not work?
+    os.system("rm -rf ${POWR_WORK}/scratch")
+    os.system("rm -rf ${POWR_WORK}/output")
+    os.system("rm -rf ${POWR_WORK}/wrdata1")
+    os.system("rm -rf ${POWR_WORK}/wrjobs")
+    return "Cleaned working dirs"
--- test/test_colitest.py
+++ test/test_colitest.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-from pathlib import Path
 import pytest
 import numpy as np
 
@@ -21,56 +20,6 @@ MODEL_DATA_REF = np.array(
 )
 
 
-# get the path of this file and export variables accordingly
-@pytest.fixture(scope="module")
-def set_vars():
-    filedir = Path(__file__).parents[0]
-    powrdir = filedir.parents[0] / "powr"
-    os.environ["POWR_WORK"] = powrdir.as_posix()
-    os.environ["POWREXEPATH"] = (powrdir / "exe.dir").as_posix()
-    return powrdir
-
-
-@pytest.fixture(scope="module")
-def set_aliases(set_vars):
-    setup_file = "bash_setup"  # this could be different on MacOS
-    # the aliases setup currently does not work as the aliases
-    # are destryed between subprocess sessions
-    # either we set them in the python script or
-    # run explicitly with passing the env as dict
-    full_file_path = "${POWR_WORK}/proc.dir/" + setup_file
-    subprocess.run(
-        full_file_path,
-        shell=True,
-        check=True,
-        executable="/bin/bash",
-        capture_output=True,
-        text=True,
-    )
-
-
-@pytest.fixture(scope="module")
-def get_chain(set_aliases):
-    makechain_command = "${POWR_WORK}/proc.dir/makechain.com 1"
-    subprocess.run(
-        makechain_command,
-        shell=True,
-        check=True,
-        executable="/bin/bash",
-        capture_output=True,
-        text=True,
-    )
-    yield "Created chain 1"
-    # teardown directories
-    # we need access to ${POWR_WORK} so shutil will not work
-    # why does sourcing powrconfig in the script not work?
-    os.system("rm -rf ${POWR_WORK}/scratch")
-    os.system("rm -rf ${POWR_WORK}/output")
-    os.system("rm -rf ${POWR_WORK}/wrdata1")
-    os.system("rm -rf ${POWR_WORK}/wrjobs")
-    return "Cleaned working dirs"
-
-
 @pytest.fixture(scope="module")
 def run_colitest(get_chain):
     # need aliases and env for this to run on CI
@@ -89,6 +38,7 @@ def run_colitest(get_chain):
     os.system("cat ${POWR_WORK}/output/colitest1.cpr")
     yield "ran colitest"
     os.system("rm -rf ${POWR_WORK}/tmp_data")
+    os.system("rm -rf ${POWR_WORK}/tmp_2day/*")
     return "Cleaned colitest tmp data"
 
 
@@ -120,6 +70,8 @@ def test_makechain(set_vars, get_chain):
     wrdata1_content = [
         "CARDS",
         "DATOM",
+        "FEDAT",
+        "FEDAT_FORMAL",
         "FGRID",
         "FORMAL_CARDS",
         "MODEL",
@@ -187,5 +139,5 @@ def test_colitest_run(set_vars, run_colitest):
 def test_read_model(set_vars, run_colitest):
     model_output = set_vars / "wrdata1" / "MODEL_STUDY_DONE"
     data_np = np.fromfile(model_output, dtype=float)
-    assert len(data_np) == 2233856
+    assert len(data_np) == 2246784
     assert np.allclose(data_np[256138:256149], MODEL_DATA_REF)
