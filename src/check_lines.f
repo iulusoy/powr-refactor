109b1d764316c92717c70f30c5dfe3791f5bb217
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-01-26 10:59:40 +0100

    fix char length bounds

--- src/check_lines.f
+++ src/check_lines.f
@@ -1,12 +1,12 @@
-      SUBROUTINE CHECK_LINES(XK, XKMIN, XKMID, XKMAX, 
+      SUBROUTINE CHECK_LINES(XK, XKMIN, XKMID, XKMAX,
      >             LINECHECK, ILINECHECK, NLINE, MAXLIN, LEVEL,
-     >             LIND, LINDS, WS, BPLOT, RADIUS, NLACT, LINE, 
+     >             LIND, LINDS, WS, BPLOT, RADIUS, NLACT, LINE,
 C***  for PRELINECL
      >             NUP, LOW, N, XLAM, NDIM, ND, XJLMEAN, XLAMAPPMEAN,
      >             bALOTri, XLAMAPPUMEAN, XLAMAPPLMEAN,
-     >             ELEVEL, INDNUP, INDLOW, NDDIM, 
+     >             ELEVEL, INDNUP, INDLOW, NDDIM,
 C***  and also for LIOP
-     >             EINST, WEIGHT, XLAMSOR, ENTOT, POPNUM, RSTAR, 
+     >             EINST, WEIGHT, XLAMSOR, ENTOT, POPNUM, RSTAR,
      >             OPAL, ETAL, VDOPUNIT, hMODEL, hALO)
 C****************************************************************
 C***  Adds new Lines to the List of Active Lines
@@ -14,8 +14,8 @@ C***    The Line Opacities are prepared
 C***  Deletes Lines which are finished from the List
 C***    The J-bars are writted to the Model File
 C***
-C***  Active lines are 1 ... NLACT and identified via the 
-C***  LIND array. LIND returns the IND (line index) for an 
+C***  Active lines are 1 ... NLACT and identified via the
+C***  LIND array. LIND returns the IND (line index) for an
 C***  active transition. LIND == 0 means that this entry is
 C***  unused. Note that due to additions and removals LIND is
 C***  not necessarily filled as a block, but instead might have
@@ -24,8 +24,9 @@ C***
 C***    Called by COLI
 C****************************************************************
 
-      INTEGER, INTENT(IN) :: hMODEL, hALO
+      INTEGER, INTENT(IN) :: hMODEL, hALO, NDDIM
       LOGICAL, INTENT(IN) :: bALOTri
+
 
       DIMENSION XKMIN(NLINE), XKMID(NLINE), XKMAX(NLINE), LINE(NLINE)
       DIMENSION LIND(MAXLIN), LINDS(MAXLIN), WS(NDDIM, MAXLIN)
@@ -42,12 +43,12 @@ C****************************************************************
 
       CHARACTER(8) :: NAME
       CHARACTER(10), DIMENSION(NDIM) :: LEVEL
-      
+
       REAL, PARAMETER :: ALOMAX = 0.9999999999
 
 C***  At the first call of this routine we have
 C***  LINECHECK = 1  and  ILINECHECK = LINE(LINECHECK)
-C***  This has been prepared in the subroutine PREPK, 
+C***  This has been prepared in the subroutine PREPK,
 C***  together with the XKMIN and XKMAX arrays.
 
 C***  In subsequent call, LINECHECK always keeps its current
@@ -55,7 +56,7 @@ C***  value. This is possible since the lines are ordered by
 C***  wavelengths and thus each line has only one wavelength
 C***  regime where it is active. (I.e., once its active region
 C***  has passed, it will never be active again!)
-      
+
 
 C****************************************************************
 C***  Check the currently active lines if they can be checked out
@@ -89,21 +90,21 @@ C***          Limit to values between 0 and ALOMAX:
               WRITE (NAME,'(A3,I5)') 'XJL',LACT
             ENDIF
 C***        Write J_L_bar into MODEL file
-            CALL WRITMS(hMODEL, 
+            CALL WRITMS(hMODEL,
      >        XJLMEAN(1,NL), ND, NAME, -1, IDUMMY, IERR)
 C***        empty entries are marked by LIND=0
 C***        Write freq.-integrated ALO into ALO file
             WRITE (NAME,'(A3,I5)') 'ALO', LACT
-            CALL WRITMS(hALO, 
+            CALL WRITMS(hALO,
      >        XLAMAPPMEAN(1,NL), ND, NAME, -1, IDUMMY, IERR)
 C***        Tri-diagonal ALO is only calculated and stored
 C***          if requested via CARDS line (XJLAPP COLI TRI)
             IF (bALOTri) THEN
               WRITE (NAME,'(A3,I5)') 'ALU', LACT
-              CALL WRITMS(hALO, 
+              CALL WRITMS(hALO,
      >          XLAMAPPUMEAN(1,NL), ND, NAME, -1, IDUMMY, IERR)
               WRITE (NAME,'(A3,I5)') 'ALL', LACT
-              CALL WRITMS(hALO, 
+              CALL WRITMS(hALO,
      >          XLAMAPPLMEAN(1,NL), ND, NAME, -1, IDUMMY, IERR)
             ENDIF
             LIND(NL) = 0
