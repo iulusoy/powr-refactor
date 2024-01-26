014a5c5c3cd23017ef7c182d30b1e1bd409391c1
Horea Caramizaru <nash_mit@yahoo.com>
2024-01-26 10:58:54 +0100

    Adding tests (#12)
    * add try / except
    
    * switch to 2 cores
    
    * siwtch back to one core compilation in CI
    
    * add tests for colitest1.cpr
    
    * add tests
    
    * fix assert
    
    * fix test
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix tests
    
    * fix try-except's blocks
    
    * fixture to set the base path of 'data' folder
    
    * add test for plot
    
    * rename fuction
    
    * change function name
    
    * remove gibberish line
    
    * add flake8 config for precommit hook
    
    * comment out offending test
    
    * delete unnecessary tests
    
    * delete unnecessary string comparison
    
    * add extra tests
    
    * update tests
    
    * add integration test test_run_wrstart
    
    * also run nonopt
    
    * add packages for pre-commit also into requirements
    
    * do not use nonopt in integration tests for now-first some refactor of programs is needed
    
    * remove trailing whitespace
    
    * some more trailing whitespace and set codecov project settings
    
    ---------
    
    Co-authored-by: Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>

--- test/test_colitest.py
+++ test/test_colitest.py
@@ -24,21 +24,26 @@ MODEL_DATA_REF = np.array(
 def run_colitest(get_chain, colitest_options):
     # need aliases and env for this to run on CI
     # run colitest
+
     # here we need to run with and without nonopt
     if colitest_options is None:
         colitest_options = ""
     colitest_path = "${POWR_WORK}/wrjobs/colitest1"
     colitest_command = colitest_path + colitest_options
-    temp = subprocess.run(
-        colitest_command,
-        shell=True,
-        check=True,
-        executable="/bin/bash",
-        capture_output=True,
-        text=True,
-    )
-    print(temp.stdout)
-    print(temp.stderr)
+    try:
+        subprocess.run(
+            colitest_command,
+            shell=True,
+            check=True,
+            executable="/bin/bash",
+            capture_output=True,
+            text=True,
+        )
+    except subprocess.CalledProcessError as error:
+        print(error.stderr)
+        print(error.stdout)
+        assert False, "CalledProcessError error"
+
     os.system("cat ${POWR_WORK}/output/colitest1.cpr")
     yield "ran colitest"
     os.system("rm -rf ${POWR_WORK}/tmp_data")
@@ -84,6 +89,7 @@ def test_makechain(set_vars, get_chain):
     ]
     wrjobs_content = [
         "coli_test",
+        "colitest1",
         "formal_wrh_gen",
         "formal_wrh_xxl",
         "newformal_cards1",
@@ -92,7 +98,6 @@ def test_makechain(set_vars, get_chain):
         "wrstart_wrh_hydro",
         "wruniq1",
         "wruniq_wrh_merged",
-        "colitest1",
         "formal_wrh_hydro",
         "modify1",
         "newformal_cards_gen",
@@ -129,15 +134,37 @@ def test_makechain(set_vars, get_chain):
     assert sorted(temp) == sorted(wrjobs_content)
 
 
+def extract_np_between(str, start, end):
+    partition = str.partition(start)
+    plot = partition[2].partition(end)[0]
+    plot_np = np.fromstring(plot, sep=" ")
+
+    return plot_np
+
+
 # check that colitest run produces correct output
 @pytest.mark.parametrize("colitest_options", ["", " nonopt"])
-def test_colitest_run(set_vars, run_colitest):
+def test_colitest(set_vars, get_plot_to_match, run_colitest):
+    strs_searched_out = [
+        "Maximum Opacity at Depth 1: K= 41042;",
+        "Lambda=     303.771;",
+        "Opacity= 0.15260E+05;",
+        "Opacities set to 0.01 * Background Opacity at    198 frequencies",
+        "SMALLPOP=   0.100E-07 chosen different from recommended default   0.100E-11",
+        "NEGATIVE BOUND-BOUND COLLISIONAL CROSS SECTION DETECTED (LEVELS: UP=   5, LOW=   2)",
+        "MODEL START 21/07/02    16:50:14    70795/0.4D/1600 L=5.3 N=1.5% C=1E-4 Fe=1.4E-3 D4 WNE 10-17",
+        "GAMMAC=     80.0   GAMMAL=    800.0",
+        "DELTAC=     -1.0   GAMMAR=    800.0   GAMMAD=      0.0",
+        "CORRECTIONS REDUCED BY FACTOR 0.50",
+    ]
+
     # check that output/colitest1.cpr is there
     # check that wrdata1/MODEL_STUDY_DONE is there
     colitest_output = set_vars / "output" / "colitest1.cpr"
     model_output = set_vars / "wrdata1" / "MODEL_STUDY_DONE"
     assert colitest_output.is_file()
     assert model_output.is_file()
+
     # compare the output from the colitest job
     model_output = set_vars / "wrdata1" / "MODEL_STUDY_DONE"
     data_np = np.fromfile(model_output, dtype=float)
@@ -145,3 +172,66 @@ def test_colitest_run(set_vars, run_colitest):
     # the original file length is 2233856, so if the coli job does not run
     # the test will fail and the model file length will be 2233856
     assert np.allclose(data_np[256138:256149], MODEL_DATA_REF)
+
+    colitest_file = set_vars / "output/colitest1.out"
+
+    with open(colitest_file, "r") as f:
+        output_for_test = f.read()
+
+    for str_searched in strs_searched_out:
+        assert str_searched in output_for_test
+
+    ###########################################################################
+
+    colitest_plot = set_vars / "output/colitest1.plot"
+
+    with open(colitest_plot, "r") as f:
+        plot_for_test = f.read()
+
+    plot_np0 = extract_np_between(
+        plot_for_test, "N= 1388   PLOTSYMBOL=  5", "N=   1388 COLOR=2"
+    )
+    plot_values0 = np.fromstring(get_plot_to_match[0], sep=" ")
+    assert np.allclose(plot_np0, plot_values0, atol=1e-06)
+
+    plot_np1 = extract_np_between(plot_for_test, " N=   1388 COLOR=2", "ENDE")
+    plot_values1 = np.fromstring(get_plot_to_match[1], sep=" ")
+    assert np.allclose(plot_np1, plot_values1, atol=1e-06)
+
+    plot_np2 = extract_np_between(
+        plot_for_test, "N=     51 COLOR= 2 PEN = 3", "N=      2 COLOR=3"
+    )
+    plot_values2 = np.fromstring(get_plot_to_match[2], sep=" ")
+    assert np.allclose(plot_np2, plot_values2, atol=1e-06)
+
+    plot_np3 = extract_np_between(
+        plot_for_test, "N=     50 PEN=4 COLOR=2", "N=     50 SYMBOL=5 COLOR=4"
+    )
+    plot_values3 = np.fromstring(get_plot_to_match[3], sep=" ")
+    assert np.allclose(plot_np3, plot_values3, atol=1e-06)
+
+    plot_np4 = extract_np_between(
+        plot_for_test, " N=     50 SYMBOL=5 COLOR=4", "N=     50 SYMBOL=5 COLOR=9"
+    )
+    plot_values4 = np.fromstring(get_plot_to_match[4], sep=" ")
+    assert np.allclose(plot_np4, plot_values4, atol=1e-06)
+
+    plot_np5 = extract_np_between(
+        plot_for_test, "N=     50 SYMBOL=5 COLOR=9", "N=     50 SYMBOL=9 SIZE=0.2"
+    )
+    plot_values5 = np.fromstring(get_plot_to_match[5], sep=" ")
+    assert np.allclose(plot_np5, plot_values5, atol=1e-06)
+
+    plot_np6 = extract_np_between(
+        plot_for_test, "N=     50 SYMBOL=9 SIZE=0.2", "N=     50 SYMBOL=10 SIZE=0.2"
+    )
+    plot_values6 = np.fromstring(get_plot_to_match[6], sep=" ")
+    assert np.allclose(plot_np6, plot_values6, atol=1e-06)
+
+    plot_np7 = extract_np_between(plot_for_test, "N=     50 SYMBOL=10 SIZE=0.2", "ENDE")
+    plot_values7 = np.fromstring(get_plot_to_match[7], sep=" ")
+    assert np.allclose(plot_np7, plot_values7, atol=1e-06)
+
+    plot_np12 = extract_np_between(plot_for_test, "N=   49   PLOTSYMBOL=  1", "ENDE")
+    plot_values12 = np.fromstring(get_plot_to_match[8], sep=" ")
+    assert np.allclose(plot_np12, plot_values12, atol=1e-06)
--- test/test_wrstart.py
+++ test/test_wrstart.py
@@ -1,31 +1,55 @@
 import os
 import subprocess
 import pytest
+import time
 
-# import numpy as np
+import numpy as np
+
+
+def find_string(str, file, set_vars):
+    file = set_vars / "output" / file
+
+    try:
+        with open(file, "r") as f:
+            output_for_test = f.read()
+        return str in output_for_test
+    except FileNotFoundError:
+        return False
 
 
 @pytest.fixture()
-def run_wrstart(get_chain, submit_options):
+def run_wrstart(get_chain, set_vars, submit_options):
     if submit_options is None:
         submit_options = ""
-    # we actually need to wait for submit to finish
+    # we actually need to wait for subvarsmit to finish
     # wrstart finishes first then wruniq
     # for wruniq, in wruniq1.log there appears a line
     # ""
     submit_path = "${POWR_WORK}/proc.dir/submit.com wrstart1"
     submit_command = submit_path + submit_options
     # run wrstart
-    temp = subprocess.run(
-        submit_command,
-        shell=True,
-        check=True,
-        executable="/bin/bash",
-        capture_output=True,
-        text=True,
-    )
-    print(temp.stdout)
-    print(temp.stderr)
+
+    try:
+        subprocess.run(
+            submit_command,
+            shell=True,
+            check=True,
+            executable="/bin/bash",
+            capture_output=True,
+            text=True,
+        )
+    except subprocess.CalledProcessError as error:
+        print(error.stderr)
+        print(error.stdout)
+        assert False, "CalledProcessError error"
+
+    while True:
+        if find_string("WRSTART finished", "wrstart1.log", set_vars) and find_string(
+            "wruniq: model finally converged", "wruniq1.log", set_vars
+        ):
+            break
+        time.sleep(1)
+
     os.system("ls ${POWR_WORK}")
     os.system("ls ${POWR_WORK}/wrdata1")
     os.system("ls ${POWR_WORK}/output")
@@ -35,6 +59,107 @@ def run_wrstart(get_chain, submit_options):
     return "Cleaned powr tmp data"
 
 
-@pytest.mark.parametrize("submit_options", ["", " nonopt"])
-def test_run_wrstart(run_wrstart):
-    print("done")
+def extract_np_between(str, start, end):
+    partition = str.partition(start)
+    plot = partition[2].partition(end)[0]
+    plot_np = np.fromstring(plot, sep=" ")
+
+    return plot_np
+
+
+# @pytest.mark.parametrize("submit_options", ["", " nonopt"])
+@pytest.mark.parametrize("submit_options", [""])
+def test_run_wrstart(
+    run_wrstart,
+    set_vars,
+    get_wrstart1_out_to_match,
+    get_wrstart1_cpr_to_match,
+    get_wrstart1_plot_to_match,
+    get_wruniq1_cpr_to_match,
+    get_wruniq1_out_to_match,
+    get_wruniq1_plot_to_match,
+):
+    wrstart1_out_file = set_vars / "output/wrstart1.out"
+
+    with open(wrstart1_out_file, "r") as f:
+        output_for_test = f.read()
+
+    for idx, str_searched in enumerate(get_wrstart1_out_to_match):
+        assert (
+            str_searched in output_for_test
+        ), f"String {idx=} not found in get_wrstart1_out_to_match"
+
+    ##########
+
+    wrstart1_cpr_file = set_vars / "output/wrstart1.cpr"
+
+    with open(wrstart1_cpr_file, "r") as f:
+        output_for_test = f.read()
+
+    for idx, str_searched in enumerate(get_wrstart1_cpr_to_match):
+        assert (
+            str_searched in output_for_test
+        ), f"String {idx=} not found in get_wrstart1_cpr_to_match"
+
+    # ##########
+
+    wrstart1_plot_file = set_vars / "output/wrstart1.plot"
+
+    with open(wrstart1_plot_file, "r") as f:
+        output_for_test = f.read()
+
+    for idx, str_searched in enumerate(get_wrstart1_plot_to_match):
+        assert (
+            str_searched in output_for_test
+        ), f"String {idx=} not found in get_wrstart1_plot_to_match"
+
+    #############
+
+    wruniq1_cpr_file = set_vars / "output/wruniq1.cpr"
+
+    with open(wruniq1_cpr_file, "r") as f:
+        output_for_test = f.read()
+
+    for idx, str_searched in enumerate(get_wruniq1_cpr_to_match):
+        assert (
+            str_searched in output_for_test
+        ), f"String {idx=} not found in get_wruniq1_cpr_to_match"
+
+    ##############
+
+    wruniq1_out_file = set_vars / "output/wruniq1.out"
+
+    with open(wruniq1_out_file, "r") as f:
+        output_for_test = f.read()
+
+    for idx, str_searched in enumerate(get_wruniq1_out_to_match):
+        assert (
+            str_searched in output_for_test
+        ), f"String {idx=} not found in get_wruniq1_out_to_match"
+
+    ###############
+
+    wruniq1_plot = set_vars / "output/wruniq1.plot"
+
+    with open(wruniq1_plot, "r") as f:
+        plot_for_test = f.read()
+
+    plot_np0 = extract_np_between(plot_for_test, "N=? XYTABLE COLOR=2 PEN=3", "FINISH")
+    plot_values0 = np.fromstring(get_wruniq1_plot_to_match[0], sep=" ")
+    assert np.allclose(plot_np0, plot_values0, atol=1e-06)
+
+    plot_np1 = extract_np_between(plot_for_test, "N= 1388   PLOTSYMBOL=  5", "FINISH")
+    plot_values1 = np.fromstring(get_wruniq1_plot_to_match[1], sep=" ")
+    assert np.allclose(plot_np1, plot_values1, atol=1e-06)
+
+    plot_np2 = extract_np_between(plot_for_test, "N=   1388 COLOR=2", "ENDE")
+    plot_values2 = np.fromstring(get_wruniq1_plot_to_match[2], sep=" ")
+    assert np.allclose(plot_np2, plot_values2, atol=1e-06)
+
+    plot_np3 = extract_np_between(
+        plot_for_test,
+        "N=     49 COLOR=3 SYMBOL=9 SIZE=0.1",
+        "N=     49 COLOR=7 SYMBOL=9 SIZE=0.1",
+    )
+    plot_values3 = np.fromstring(get_wruniq1_plot_to_match[3], sep=" ")
+    assert np.allclose(plot_np3, plot_values3, atol=1e-06)
